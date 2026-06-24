ffa9d2c18c4d3f5426c78e30031173a191de03a8
Sofiia <sofiiamuz@gmail.com>
2026-06-25 00:01:14 +0200

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,32 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post, comments }) => {
+  const filteredComments = comments.filter(
+    comment => comment.postId === post.id,
+  );
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <UserInfo user={post.user} key={post.id} />
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {filteredComments.length === 0 && (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+
+      {filteredComments.length !== 0 && (
+        <CommentList comments={filteredComments} />
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,13 @@
-export const PostList = () => <>Put the list here</>;
+// import { comments } from '../../App';
+// import { CommentList } from '../CommentList/CommentList';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, comments }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <div key={post.id}>
+        <PostInfo post={post} comments={comments} />
+      </div>
+    ))}
+  </div>
+);
